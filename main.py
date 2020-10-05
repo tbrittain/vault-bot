abe5e09df9df3c2374da52bd4b6c49c4d18c93aa
tbrittain <treybrittain@gmail.com>
2020-10-05 00:22:24 -0500

    Search and Add functions complete. Working on ensuring songs aged 2 weeks or greater are removed from playlist.

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ from dotenv import load_dotenv
 import asyncio
 import spotify_commands
 import time
+from spotipy import SpotifyException
+import schedule
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
 bot = discord.Client()
 
+
+# TODO: make bot ignore messages from other bots
 
 @bot.event
 async def on_ready():
@@ -27,26 +31,123 @@ async def on_ready():
 async def on_message(message):
     if message.content[0] == "$":  # $ dollar sign will be the default bot command
         user_input = str(message.content)
-
+        channel = message.channel
         print(time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()) + f' User {message.author} invoked = {user_input}')
 
         user_input = user_input.replace("$", "")  # bot already called, removing $ for parsing user message
         first_word = user_input.split(' ', 1)[0]  # first word corresponds to command
-        message_body = user_input.split(' ', 1)[1:]  # rest of body correspond to command argument(s)
+        try:
+            message_body = user_input.split(' ', 1)[1:]  # rest of body correspond to command argument(s)
+        except IndexError:
+            pass  # some commands do not have arguments, so ignore
 
         # begin commands
-        if first_word == 'search':
+        if first_word == 'search':  # $search command
             if not message_body:  # handle if no arguments for search
                 await message.channel.send(f'Please search for a song with your words, {message.author.mention} ')
             else:
-                search_results = await asyncio.gather(spotify_commands.song_search(message_body))
-                await message.channel.send(embed=search_results)
-                await message.channel.send("Use the number of your desired track in the $add command")
-        elif first_word == 'add':
-            await message.channel.send("add")
+                raw_results = await asyncio.gather(spotify_commands.song_search(message_body))
+                search_results = raw_results[0]
+                msg = await message.channel.send(search_results[0])
+
+                track_ids = search_results[1]
+                print(len(track_ids))
+                print(track_ids)
+
+                emojis = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣',
+                          '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟']
+
+                emoji_counter = 0
+                for emoji in emojis:  # only adds emojis on the results message for the amount of tracks present
+                    if emoji_counter == len(track_ids):
+                        break
+                    else:
+                        await msg.add_reaction(emoji=emoji)
+                        emoji_counter += 1
+                await message.channel.send(f'Select the emoji of the track you want to add, {message.author.mention}')
+
+                def check(reaction, user):  # define the conditions where the bot will actually add the song
+                    if user == message.author and any(str(reaction.emoji) in s for s in emojis):
+                        return True
+                    else:
+                        return False
+
+                try:
+                    reaction, user = await bot.wait_for('reaction_add', timeout=60.0, check=check)
+                except asyncio.TimeoutError:
+                    await message.channel.send(f'Nevermind, {message.author.mention}. Be that way.')
+                else:  # song added to playlist here
+                    if reaction.emoji == '1️⃣':
+                        track_selection = 0
+                    elif reaction.emoji == '2️⃣':
+                        track_selection = 1
+                    elif reaction.emoji == '3️⃣':
+                        track_selection = 2
+                    elif reaction.emoji == '4️⃣':
+                        track_selection = 3
+                    elif reaction.emoji == '5️⃣':
+                        track_selection = 4
+                    elif reaction.emoji == '6️⃣':
+                        track_selection = 5
+                    elif reaction.emoji == '7️⃣':
+                        track_selection = 6
+                    elif reaction.emoji == '8️⃣':
+                        track_selection = 7
+                    elif reaction.emoji == '9️⃣':
+                        track_selection = 8
+                    elif reaction.emoji == '🔟':
+                        track_selection = 9
+                    print(track_ids[track_selection][track_selection + 1])
+                    selected_track_id = track_ids[track_selection][track_selection + 1]
+                    await message.channel.send('👍')
+                    await spotify_commands.add_to_playlist(selected_track_id)
+                    await message.channel.send('Track has been added to the community playlist!')
+
+        elif first_word == 'add':  # $add command
+            message_body = str(message_body)
+            message_body = message_body.replace('[', "")
+            message_body = message_body.replace(']', "")
+            message_body = message_body.replace("'", "")
+            try:
+                await spotify_commands.add_to_playlist(message_body)
+            except IndexError:
+                await message.channel.send(f'Please enter a Spotify track ID, {message.author.mention}')
+            except SpotifyException:
+                await message.channel.send(f'Please enter a valid argument, {message.author.mention}')
+                await message.channel.send(f'Valid arguments for $add are the raw Spotify song link, song URI, '
+                                           f'or song ID')
+
+        elif first_word == 'playlist':
+            try:
+                playlist_embed = discord.Embed(title='$playlist',
+                                               description='Links to the playlists',
+                                               color=0x00ff00)
+                playlist_embed.add_field(name='Main playlist', value='https://open.spotify.com/playlist'
+                                                                     '/5YQHb5wt9d0hmShWNxjsTs?si=Zw8fNLiKSligb1pQcMfUlg')
+                playlist_embed.add_field(name='Archive playlist', value='https://open.spotify.com/playlist'
+                                                                        '/4C6pU7YmbBUG8sFFk4eSXj?si=R2O3G3suS1CSuFEQPjZIhA')
+                await message.channel.send(embed=playlist_embed)
+            except IndexError:
+                pass
+
+        elif first_word == 'help':
+            try:
+                help_embed = discord.Embed(title='$help',
+                                           description='Hopefully this answers your question...',
+                                           color=0x00ff00)
+                help_embed.add_field(name='$search', value='Searches Spotify based on song name '
+                                                           'and lists the top 10 results')
+                help_embed.add_field(name='$add', value='Input a Spotify song link/URI/ID to add it directly to the '
+                                                        'playlist')
+                help_embed.add_field(name='$playlist', value='Pastes link to the Spotify playlist')
+                await message.channel.send(embed=help_embed)
+            except IndexError:
+                pass
         else:
             await message.channel.send(f'Unrecognized command "{first_word}", {message.author.mention}!')
 
 
 if __name__ == "__main__":
     bot.run(DISCORD_TOKEN)
+    # after this, need to invoke command to analyze whether song been added >2 weeks ago, then remove it from dynamic
+    # playlist
--- spotify_commands.py
+++ spotify_commands.py
@@ -1,15 +1,20 @@
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 import os
 import asyncio
+import time
+from datetime import datetime, timedelta
 
 load_dotenv()
 CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
 CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
+REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
 
 scope = "playlist-modify-public"
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET))
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
+                                               client_secret=CLIENT_SECRET,
+                                               redirect_uri=REDIRECT_URI))
 
 
 async def song_search(user_message):
@@ -21,31 +26,83 @@ async def song_search(user_message):
     # print(search_results['tracks']['items'])
     if len(search_results['tracks']['items']) > 0:
         track_results_string = "```"
+        results_list = []
         counter = 1
         for item in search_results['tracks']['items']:
             artist = (item['album']['artists'][0]['name'])  # artist name
             album = (item['album']['name'])  # album name
             track = (item['name'])  # track name
 
-            artist_url = (item['album']['artists'][0]['external_urls']['spotify'])
-            album_url = (item['album']['external_urls']['spotify'])
+            track_id = (item['id'])
+            track_id_dic = {counter: track_id}
+            results_list.append(track_id_dic)
+            # TODO: figure out how to embed links in markdown
+            # https://stackoverflow.com/questions/44862112/how-can-i-send-an-embed-via-my-discord-bot-w-python
+            # https://support.discord.com/hc/en-us/articles/210298617-Markdown-Text-101-Chat-Formatting-Bold-Italic-Underline-
+            # artist_url = (item['album']['artists'][0]['external_urls']['spotify'])
+            # album_url = (item['album']['external_urls']['spotify'])
 
-            track_results_string += f'{counter}. {track} by [{artist}]({artist_url})' \
-                                    f' from album [{album}]({album_url})\n'
+            track_results_string += f'{counter}. "{track}" by {artist} from album "{album}"\n'
             counter += 1
         track_results_string += '```'
-        return track_results_string
+        return [track_results_string, results_list]
     else:
         return f'No Spotify tracks for query {original_message} found!'
 
 
-async def add_to_playlist(user_message):
-    await asyncio.gather(song_search())
+# TODO: add sqlite database or JSON integration here to track overall stats
+async def add_to_playlist(song_id):
+    song_id = [song_id, ]
+    sp.playlist_add_items('5YQHb5wt9d0hmShWNxjsTs', song_id)
+    print(time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()) +
+          f'Song of ID {song_id} added to Vault Community Playlist')
+    sp.playlist_add_items('4C6pU7YmbBUG8sFFk4eSXj', song_id)
+    print(time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()) +
+          f'Song of ID {song_id} added to Vault Community Archive Playlist')
 
-    return
+
+def song_time_check():
+    results = sp.playlist_items(playlist_id='5YQHb5wt9d0hmShWNxjsTs')
+    tracks = results['items']
+    while results['next']:
+        results = sp.next(results)
+        tracks.extend(results['items'])
+
+    track_list = []
+    if len(tracks) > 0:
+        for track in tracks:
+            # date in YYYY-MM-DD format by default from Spotify
+            added_at = track['added_at']
+            added_at = added_at.split('T', 1)
+            added_at = added_at[0]
+            track_dict = {track['track']['id']: added_at}
+
+            track_list.append(track_dict)
+    for track in track_list:
+        for key, value in track.items():
+            date_split = value.split('-')
+            time_difference = datetime.now() - datetime(year=int(date_split[0]),
+                                                        month=int(date_split[1]),
+                                                        day=int(date_split[2]))
+            print(time_difference)
+
+# https://stackoverflow.com/a/61529490/14338656
+def octToDec(octal_number):
+    string_oct = str(octal_number)
+    le = len(string_oct)
+    octal = 0
+    for i in (range(le)):
+        octal = octal + int(string_oct[i]) * pow(8, le - 1)
+        le -= 1
+    print(octal)
 
 
 if __name__ == "__main__":
-    search = input('Input a song name: ')
-    results = asyncio.run(song_search(search))
-    print(results)
+    song_time_check()
+    # print(datetime.now())
+    # print(datetime(year=2020, month=10, day=int(5)))
+    # d = datetime.now() - datetime(year=2020, month=9, day=int(5))
+    # if d > timedelta(days=14):
+    #     print('This returned true')
+    #     print(d)
+
