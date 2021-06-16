19f0801167ff66f652d89088277ac72e29e36b08
tbrittain <treybrittain@gmail.com>
2021-06-15 22:29:38 -0500

    Ordered Discord bot in its respective folder

--- config.py
+++
@@ -1,4 +0,0 @@
-# going to store commonly used variables in this config file
-# considering getting rid of this file and just storing all variables in .env
-bot_command_prefix = '$'
-environment = 'dev'
