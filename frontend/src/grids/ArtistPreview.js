3b2654ccdcc0cb139d6c46e29d67df3a06adc2ee
tbrittain <treybrittain@gmail.com>
2021-12-29 23:47:38 -0600

    Default avatar for artists without image

--- frontend/src/grids/ArtistPreview.js
+++ frontend/src/grids/ArtistPreview.js
@@ -1,43 +1,28 @@
-import React from 'react'
-import {
-  Paper,
-  Tooltip,
-  Fade
-  // Backdrop
-} from '@material-ui/core'
-import { Link } from 'react-router-dom'
-import { LazyLoadImage } from 'react-lazy-load-image-component'
-import gridStyles from './GridStyles'
-import { makeStyles } from '@material-ui/core/styles'
-import 'react-lazy-load-image-component/src/effects/opacity.css'
+import React from "react";
+import { Avatar, Fade, Paper, Tooltip } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import gridStyles from "./GridStyles";
+import { makeStyles } from "@material-ui/core/styles";
+import "react-lazy-load-image-component/src/effects/opacity.css";
 
 const darkTooltipTheme = makeStyles((theme) => ({
   tooltip: {
-    backgroundColor: 'rgba(0, 0, 0, 0.78)',
+    backgroundColor: "rgba(0, 0, 0, 0.78)",
     fontSize: theme.typography.pxToRem(16),
-    fontWeight: 800
-  }
-}))
+    fontWeight: 800,
+  },
+}));
 
 const DarkTooltip = (props) => {
-  const classes = darkTooltipTheme()
+  const classes = darkTooltipTheme();
 
-  return <Tooltip classes={classes} {...props} />
-}
+  return <Tooltip classes={classes} {...props} />;
+};
 
 const ArtistPreview = (props) => {
-  const { name, id, art } = props
-  const classes = gridStyles()
-
-  // const [open, setOpen] = useState(false)
-
-  // const handleMouseEnter = () => {
-  //   setOpen(true)
-  // }
-
-  // const handleMouseLeave = () => {
-  //   setOpen(false)
-  // }
+  const { name, id, art } = props;
+  const classes = gridStyles();
 
   return (
     <>
@@ -51,24 +36,36 @@ const ArtistPreview = (props) => {
           className={classes.artistCard}
           component={Link}
           to={`/artists/${id}`}
-          // onMouseEnter={handleMouseEnter}
-          // onMouseLeave={handleMouseLeave}
         >
-          <LazyLoadImage
-            src={art}
-            alt={name}
-            className={classes.artistArt}
-            effect='opacity'
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center'
-            }}
-          />
+          {!art && (
+            <Avatar
+              alt={name}
+              className={classes.artistArt}
+              variant="square"
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            />
+          )}
+          {art && (
+            <LazyLoadImage
+              src={art}
+              alt={name}
+              className={classes.artistArt}
+              effect="opacity"
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            />
+          )}
         </Paper>
       </DarkTooltip>
     </>
-  )
-}
+  );
+};
 
-export default ArtistPreview
+export default ArtistPreview;
