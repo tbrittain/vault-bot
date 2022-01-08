95639b75dbe267cd7c348787fc3b52b5805bff8f
tbrittain <treybrittain@gmail.com>
2022-01-07 21:45:50 -0600

    Fixed typeface issue

--- frontend/src/themes/DarkTheme.js
+++ frontend/src/themes/DarkTheme.js
@@ -2,7 +2,7 @@ import { createTheme } from "@mui/material";
 import "@fontsource/rubik/300.css";
 import "@fontsource/rubik/400.css";
 import "@fontsource/rubik/500.css";
-import "@fontsource/rubik/700.css";
+import "@fontsource/rubik/800.css";
 
 const darkTheme = createTheme({
   palette: {
