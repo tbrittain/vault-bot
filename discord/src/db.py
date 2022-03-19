f16e6571a243ec60e2911e0a37a6dab70f35c59c
trey.brittain <trey.brittain@entranceconsulting.com>
2022-03-19 18:58:56 -0500

    Hotfix: update version of secret used

--- discord/src/db.py
+++ discord/src/db.py
@@ -26,7 +26,8 @@ elif environment == "prod":
     db_pass = access_secret_version(secret_id="vb-postgres-pass",
                                     project_id=project_id)
     db_host = access_secret_version(secret_id="vb-postgres-db-host",
-                                    project_id=project_id)
+                                    project_id=project_id,
+                                    version_id="2")
     db_port = access_secret_version(secret_id="vb-postgres-db-port",
                                     project_id=project_id)
     db_name = access_secret_version(secret_id="vb-postgres-db-name",
