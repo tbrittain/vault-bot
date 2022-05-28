29501df83ad34f6a74e6665fabd771a16565ad9c
tbrittain <treybrittain@gmail.com>
2022-05-28 17:19:46 -0500

    Add ArtistSong.model.ts

---
+++ backend/src/db/models/ArtistSong.model.ts
@@ -0,0 +1,29 @@
+import {
+  Column,
+  ForeignKey,
+  Model,
+  PrimaryKey,
+  Table
+} from 'sequelize-typescript'
+import Artist from './Artist.model'
+import { ITableOptions } from './interfaces/ITableOptions'
+import Song from "./Song.model";
+
+const ArtistSongOptions: ITableOptions = {
+  tableName: 'artists_songs',
+  timestamps: false,
+  underscored: true
+}
+
+@Table(ArtistSongOptions)
+export default class ArtistSong extends Model {
+  @PrimaryKey
+  @Column
+  @ForeignKey(() => Artist)
+  artistId!: string
+
+  @PrimaryKey
+  @Column
+  @ForeignKey(() => Song)
+  songId!: string
+}
