d32c076c5e1a08586de75b47a4d8b933821251d8
tbrittain <treybrittain@gmail.com>
2022-05-28 13:29:43 -0500

    Fix archive serial key issue #172

--- discord/src/db/migrate_database.py
+++ discord/src/db/migrate_database.py
@@ -2,6 +2,7 @@ import logging
 
 from .migrations.selects_refactor import MIGRATION_ID as SELECTS_REFACTOR_MIGRATION_ID
 from .migrations.artist_song_link_table import MIGRATION_ID as ARTIST_SONG_LINK_TABLE_MIGRATION_ID
+from .migrations.archive_serial_pkey_fix import MIGRATION_ID as ARCHIVE_SERIAL_PKEY_FIX_MIGRATION_ID
 
 
 def migrate_database(cur, logger: logging.Logger):
@@ -19,3 +20,9 @@ def migrate_database(cur, logger: logging.Logger):
         from .migrations.artist_song_link_table import migration_002
         migration_002(cur)
         logger.info("Migration 002 complete")
+
+    if ARCHIVE_SERIAL_PKEY_FIX_MIGRATION_ID not in migration_ids:
+        logger.info("Running migration 003")
+        from .migrations.archive_serial_pkey_fix import migration_003
+        migration_003(cur)
+        logger.info("Migration 003 complete")
