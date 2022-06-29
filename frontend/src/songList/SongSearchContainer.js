fc704ee88c8f439586b92faab56f06c6ee6683f6
tbrittain <treybrittain@gmail.com>
2022-06-28 23:04:50 -0500

    Refactored SongSearchContainer

--- frontend/src/songList/SongSearchContainer.js
+++ frontend/src/songList/SongSearchContainer.js
@@ -1,33 +1,34 @@
-import React from "react"
-import songListStyles from "./SongListStyles"
-import { useQuery } from "@apollo/client"
-import SongSearchResult from "./SongSearchResult"
-import { Alert, Grid, Paper, Typography } from "@mui/material"
-import { SONG_SEARCH_QUERY } from "../queries/songQueries"
+import React, { useState } from 'react'
+import songListStyles from './SongListStyles'
+import { useQuery } from '@apollo/client'
+import SongSearchResult from './SongSearchResult'
+import { Alert, Grid, Paper, Typography } from '@mui/material'
+import { SONG_SEARCH_QUERY } from '../queries/songQueries'
 
 const SongSearchContainer = (props) => {
 	const classes = songListStyles()
 	const { searchQuery } = props
-	const { error, data } = useQuery(SONG_SEARCH_QUERY, {
+	const [results, setResults] = useState([])
+
+	const { error } = useQuery(SONG_SEARCH_QUERY, {
 		variables: {
 			searchQuery,
 		},
+		onCompleted: (data) => {
+			setResults(data?.findTracksLike)
+		},
 	})
-	let formattedData
-	if (data) {
-		formattedData = data.findTracksLike
-	}
 
 	if (error) {
 		return (
-			<Alert severity="error">An error occurred during data retrieval :(</Alert>
+			<Alert severity='error'>An error occurred during data retrieval :(</Alert>
 		)
 	}
 
 	return (
 		<Grid container spacing={1} className={classes.queryResultContainer}>
-			{formattedData &&
-				formattedData.map((song) => (
+			{results &&
+				results.map((song) => (
 					<SongSearchResult
 						key={song.id}
 						name={song.name}
@@ -38,14 +39,14 @@ const SongSearchContainer = (props) => {
 						searchQuery={searchQuery}
 					/>
 				))}
-			{formattedData && formattedData.length === 0 && (
+			{(results && results.length === 0) && (
 				<Grid item className={classes.songResultNoneFound}>
 					<Paper
 						style={{
 							padding: 10,
 						}}
 					>
-						<Typography variant="subtitle1">No results found :(</Typography>
+						<Typography variant='subtitle1'>No results found :(</Typography>
 					</Paper>
 				</Grid>
 			)}
