512f1498f0fe2188876e2e40e2ee9ca462cab55d
tbrittain <treybrittain@gmail.com>
2022-08-28 12:56:54 -0500

    Rework all views to use blacklist Improve power playlist query #377

--- discord/src/db/migrations/genre_table_rework.py
+++ discord/src/db/migrations/genre_table_rework.py
@@ -41,13 +41,137 @@ def migration_005(cur):
     -- Blacklisted as in never selected for Aggregate playlists
     -- See #231 https://github.com/tbrittain/vault-bot/issues/231
     ALTER TABLE artists
-    ADD is_blacklisted BOOL NOT NULL DEFAULT FALSE;
+        ADD is_blacklisted BOOL NOT NULL DEFAULT FALSE;
     
     ALTER TABLE songs
-    ADD is_blacklisted BOOL NOT NULL DEFAULT FALSE;
+        ADD is_blacklisted BOOL NOT NULL DEFAULT FALSE;
+    --#endregion
+    
+    --#region View remediation
+    UPDATE genres
+    SET is_whitelisted = TRUE
+    WHERE (genres.name::text = ANY
+           ('{"pop rap", edm, house, "tropical house", "uk dance", "deep groove house", "bass house", "electronic trap", "la pop", metropopolis, "pop edm", "big room", "vocal house", moombahton, "pop dance", "trap latino", grime, "disco house"}'::text[]));
+    
+    CREATE VIEW v_party_playlist(id, count) AS
+    SELECT s.id,
+           count(a.song_id) AS count
+    FROM songs s
+             JOIN archive a ON s.id = a.song_id
+             JOIN artists_songs ON s.id = artists_songs.song_id
+             JOIN artists a2 ON artists_songs.artist_id = a2.id
+             JOIN artists_genres ON a2.id = artists_genres.artist_id
+             JOIN genres g on artists_genres.genre_id = g.id
+    WHERE g.is_whitelisted = TRUE
+      AND s.danceability > 0.55
+      AND s.energy > 0.76
+      AND s.length < 4.25
+      AND s.tempo > 115
+      AND s.is_blacklisted = FALSE
+      AND a2.is_blacklisted = FALSE
+    GROUP BY s.name, s.id
+    ORDER BY (count(a.song_id)) DESC
+    LIMIT 100;
+    
+    DROP VIEW v_party_unfiltered_playlist;
+    CREATE VIEW v_party_unfiltered_playlist(id, count) AS
+    SELECT s.id,
+           count(a.song_id) AS count
+    FROM songs s
+             JOIN archive a ON s.id = a.song_id
+             JOIN artists_songs ON s.id = artists_songs.song_id
+             JOIN artists a2 ON artists_songs.artist_id = a2.id
+    WHERE s.danceability > 0.8
+      AND s.energy > 0.5
+      AND s.length < 4.5
+      AND s.length < 4.5
+      AND s.is_blacklisted = FALSE
+      AND a2.is_blacklisted = FALSE
+    GROUP BY s.name, s.id
+    ORDER BY (count(a.song_id)) DESC
+    LIMIT 100;
+    
+    DROP VIEW v_energy_playlist;
+    CREATE VIEW v_energy_playlist(id, count) AS
+    SELECT s.id,
+           count(a.song_id) AS count
+    FROM songs s
+             JOIN archive a ON s.id = a.song_id
+             JOIN artists_songs ON s.id = artists_songs.song_id
+             JOIN artists a2 ON artists_songs.artist_id = a2.id
+    WHERE s.energy > 0.925
+      AND loudness > -6
+      AND tempo > 115
+      AND s.is_blacklisted = FALSE
+      AND a2.is_blacklisted = FALSE
+    GROUP BY s.name, s.id
+    ORDER BY (count(a.song_id)) DESC
+    LIMIT 100;
+    
+    DROP VIEW v_chill_playlist;
+    CREATE VIEW v_chill_playlist(id, count) AS
+    SELECT s.id,
+           count(a.song_id) AS count
+    FROM songs s
+             JOIN archive a ON s.id = a.song_id
+             JOIN artists_songs ON s.id = artists_songs.song_id
+             JOIN artists a2 ON artists_songs.artist_id = a2.id
+    WHERE s.energy < 0.35
+      AND s.acousticness > 0.1
+      AND s.is_blacklisted = FALSE
+      AND a2.is_blacklisted = FALSE
+    GROUP BY s.name, s.id
+    ORDER BY (count(a.song_id)) DESC
+    LIMIT 100;
+    
+    DROP VIEW v_light_playlist;
+    CREATE VIEW v_light_playlist(id, count) AS
+    SELECT s.id,
+           count(archive.song_id) AS count
+    FROM songs s
+             JOIN archive ON s.id = archive.song_id
+             JOIN artists_songs ON s.id = artists_songs.song_id
+             JOIN artists a2 ON artists_songs.artist_id = a2.id
+    WHERE s.valence > 0.9
+      AND s.is_blacklisted = FALSE
+      AND a2.is_blacklisted = FALSE
+    GROUP BY s.name, s.id
+    ORDER BY (count(archive.song_id)) DESC
+    LIMIT 100;
+    
+    DROP VIEW v_moody_playlist;
+    CREATE VIEW v_moody_playlist(id, count) AS
+    SELECT s.id,
+           count(a.song_id) AS count
+    FROM songs s
+             JOIN archive a ON s.id = a.song_id
+             JOIN artists_songs ON s.id = artists_songs.song_id
+             JOIN artists a2 ON artists_songs.artist_id = a2.id
+    WHERE s.valence < 0.1
+      AND s.is_blacklisted = FALSE
+      AND a2.is_blacklisted = FALSE
+    GROUP BY s.name, s.id
+    ORDER BY (count(a.song_id)) DESC
+    LIMIT 100;
+    
+    DROP VIEW v_top_50_playlist;
+    CREATE VIEW v_top_50_playlist(id, count) AS
+    SELECT s.id,
+           count(a.song_id) AS count
+    FROM songs s
+             JOIN archive a ON s.id = a.song_id
+             JOIN artists_songs ON s.id = artists_songs.song_id
+             JOIN artists a2 ON artists_songs.artist_id = a2.id
+    WHERE s.is_blacklisted = FALSE
+      AND a2.is_blacklisted = FALSE
+    GROUP BY s.name, s.id
+    ORDER BY (count(a.song_id)) DESC
+    LIMIT 50;
     --#endregion
     
     INSERT INTO migration (id, description)
-    VALUES ('{MIGRATION_ID}', 'Added dedicated genres table, genres Party playlist whitelist, and overall Song/Artist 
-    aggregate playlist blacklist');
+    VALUES ('{MIGRATION_ID}',
+    'Added dedicated genres table, genres Party playlist whitelist, ' ||
+    'and overall Song/Artist aggregate playlist blacklist. Modified all ' ||
+    'views to utilize these new fields where applicable.');
     """)
