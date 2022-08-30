abe931cfb87599bfcd0a1816202d2891c6e501ef
tbrittain <treybrittain@gmail.com>
2022-08-29 22:49:23 -0500

    Update GenreList with new genre schema

--- frontend/src/queries/genreQueries.js
+++ frontend/src/queries/genreQueries.js
@@ -13,6 +13,7 @@ export const ARTISTS_FROM_GENRE_QUERY = gql`
 export const GENRE_SEARCH_QUERY = gql`
 	query genreSearchQuery($searchQuery: String!) {
 		findGenresLike(searchQuery: $searchQuery) {
+			id
 			name
 		}
 	}
@@ -21,6 +22,7 @@ export const GENRE_SEARCH_QUERY = gql`
 export const ALL_GENRES_QUERY = gql`
 	query allGenresQuery {
 		getGenres {
+			id
 			name
 			numArtists
 			rank
