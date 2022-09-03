249039b2f84bee4b7a89fb90e96f619bb5706367
tbrittain <treybrittain@gmail.com>
2022-09-03 01:07:03 -0500

    Add ArtistRank model

---
+++ backend/src/database/models/ArtistRank.model.ts
@@ -0,0 +1,37 @@
+import { ITableOptions } from './interfaces/ITableOptions'
+import {
+  BelongsTo,
+  Column,
+  ForeignKey,
+  Model,
+  Table
+} from 'sequelize-typescript'
+import Artist from './Artist.model'
+
+const ArtistRankOptions: ITableOptions = {
+  tableName: 'v_rankings_artists',
+  timestamps: false,
+  underscored: true
+}
+
+@Table(ArtistRankOptions)
+export default class ArtistRank extends Model {
+  @Column
+  @ForeignKey(() => Artist)
+  artistId!: string
+
+  @BelongsTo(() => Artist)
+  artist!: Artist
+
+  @Column
+  numUniqueSongs!: number
+
+  @Column
+  numUniqueSongsRank!: number
+
+  @Column
+  numNonUniqueSongs!: number
+
+  @Column
+  numNonUniqueSongsRank!: number
+}
