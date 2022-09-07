ec9664bed902a4aacfcf105f3f576898c92518e6
tbrittain <treybrittain@gmail.com>
2022-09-06 23:17:20 -0500

    Add handling for all the various cases of duplicate songs

--- discord/src/spotify_commands.py
+++ discord/src/spotify_commands.py
@@ -2,6 +2,7 @@ import json
 from datetime import datetime, timedelta
 from os import getenv, path
 from random import choice
+from typing import List
 from uuid import uuid4
 
 import discord.ext.commands
@@ -246,6 +247,9 @@ def add_song_to_db(song_id, user):
         table_songs_row = (song_id, song_name, song_length, tempo, dance, energy, loudness, acoustic, instrument,
                            liveness, valence, album_art, preview_url, album)
         conn.insert_single_row(table='songs', columns=table_songs_columns, row=table_songs_row)
+        # by default, insert the song as a reference to itself in the lookup
+        conn.insert_single_row(table='duplicate_song_lookup', columns=('source_song_id', 'target_song_id'),
+                               row=(song_id, song_id))
 
     else:
         conn.update_query(column_to_change="art", column_to_match="id",
@@ -333,8 +337,8 @@ def remove_songs_from_playlist(song_ids: list[str]):
 
 
 def balance_duplicate_song_lookup(song_id: str):
-    # TODO: Here, define keywords indicating non-original song
-    # i.e. remix, rework, etc.
+    def mark_target_songs_as_duplicates(target_song_id: str, source_song_ids: List[str]):
+        pass
 
     conn = DatabaseConnection()
     potential_duplicates = conn.select_query_raw(f"""
@@ -343,7 +347,7 @@ def balance_duplicate_song_lookup(song_id: str):
                s.length,
                s.tempo,
                s.album,
-               s.art,
+               s.preview_url,
                s.acousticness,
                s.danceability,
                s.liveness,
@@ -354,30 +358,88 @@ def balance_duplicate_song_lookup(song_id: str):
         WHERE "as".artist_id = ANY (SELECT as2.artist_id
                                     FROM artists_songs as2
                                     WHERE as2.song_id = '{song_id}')
+        GROUP BY s.id;
     """)
 
+    # meaning we only got our original song back
+    if len(potential_duplicates) == 1:
+        conn.terminate()
+        return
+
     initial = next(x for x in potential_duplicates if x[0] == song_id)
+    initial_is_remix = initial[1].lower().__contains__('remix')
+
     rest = [x for x in potential_duplicates if x[0] != song_id]
 
     filtered = list(filter(lambda x: abs(x[2] - initial[2]) < 0.1 and
                                      abs(x[3] - initial[3]) < 5, rest))
 
-    # TODO: here, filter out remixes if the given song is not a remix
-    # or filter to only remixes if that given song is a remix?
+    if len(filtered) == 0:
+        conn.terminate()
+        return
+
+    filtered = [x for x in filtered if x[1].lower().__contains__('remix')] if initial_is_remix \
+        else [x for x in filtered if not x[1].lower().__contains__('remix')]
+
+    if len(filtered) == 0:
+        conn.terminate()
+        return
 
     # then, out of those results, filter those down to those with significant name similarity
     filtered = list(filter(lambda x: fuzz.partial_ratio(x[1], initial[1]) > 90, filtered))
 
+    if len(filtered) == 0:
+        conn.terminate()
+        return
+
     # then filter down to results that share significant similarity with regard to song characteristics
-    # once we are at this point, then we can assume that all the results left represent the same songs
+    filtered = list(filter(lambda x: abs(x[6] - initial[6]) < 0.1 and
+                                     abs(x[7] - initial[7]) < 0.1 and
+                                     abs(x[8] - initial[8]) < 0.1 and
+                                     abs(x[9] - initial[9]) < 0.1 and
+                                     abs(x[10] - initial[10]) < 0.1, rest))
+
+    # once we are at this point, then we can assume that all the results left represent the same songs.
     # then (if more than one result) we need to select which song is the one we want to be that target
     # song for all of these duplicate results
     # priority: 1. the result(s) that have a song preview
+    combined = filtered
+    combined.append(initial)
+    all_combined_song_ids = [x[0] for x in combined]
+    filtered = list(filter(lambda x: x[5] is not None, combined))
+    if len(filtered) == 1:
+        mark_target_songs_as_duplicates(filtered[0][0], all_combined_song_ids)
+        conn.terminate()
+        return
+    elif len(filtered) == 0:
+        # reset filtered, since none of the potential target songs will have a song preview
+        filtered = combined
+
     # 2. if in different albums, the result that has the most songs in that same album
     # (which will require another trip to the database)
-    # finally, we choose that ultimate result ID to be the target ID, and we set all the found duplicate
-    # source IDs to that target ID in the lookup table
+    song_id_album_count = {}
+    for song_row in filtered:
+        album_songs = conn.select_query_raw(f"""
+        SELECT s.id, s.name, s.album
+        FROM songs s
+                 JOIN artists_songs "as" on s.id = "as".song_id
+        WHERE "as".artist_id = ANY (SELECT as2.artist_id
+                                    FROM artists_songs as2
+                                    WHERE as2.song_id = '{song_row[0]}')
+        AND s.album = '{song_row[4]}'
+        GROUP BY s.id;
+        """)
 
+        album_songs = list(filter(lambda x: x[0] not in all_combined_song_ids, album_songs))
+        song_id_album_count[song_row[0]] = len(album_songs)
+
+    max_album_count = max(song_id_album_count.values())
+    max_album_count_song_ids = [x for x, y in song_id_album_count if y == max_album_count]
+
+    # grab the first element returned in max_album_count_song_ids, regardless of whether
+    # there are more than 1, since if we get to this point, the songs that are left are
+    # by all accounts identical in all the ways that we care about
+    mark_target_songs_as_duplicates(max_album_count_song_ids[0], all_combined_song_ids)
     conn.terminate()
 
 
