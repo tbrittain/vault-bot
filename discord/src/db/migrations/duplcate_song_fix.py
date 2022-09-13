484a7eeb733c242700668a24651dc448fccc7a7a
tbrittain <treybrittain@gmail.com>
2022-09-13 16:54:35 -0500

    Add v_songs and v_archive views

--- discord/src/db/migrations/duplcate_song_fix.py
+++ discord/src/db/migrations/duplcate_song_fix.py
@@ -17,9 +17,33 @@ def migration_008(cur):
     INSERT INTO duplicate_song_lookup (source_song_id, target_song_id)
     SELECT id, id
     FROM songs;
-    
-    -- TODO: ADD NEW SONG AND ARCHIVE VIEWS IN HERE
+
+    CREATE VIEW v_songs AS
+    SELECT dsl.target_song_id AS id,
+           name,
+           length,
+           tempo,
+           danceability,
+           energy,
+           loudness,
+           acousticness,
+           instrumentalness,
+           liveness,
+           valence,
+           art,
+           preview_url,
+           album,
+           is_blacklisted
+    FROM songs s
+             JOIN duplicate_song_lookup dsl ON s.id = dsl.source_song_id AND s.id = dsl.target_song_id;
+             
+    CREATE VIEW v_archive AS
+    SELECT id, dsl.target_song_id AS song_id, added_by, added_at
+    FROM archive a
+             JOIN duplicate_song_lookup dsl ON a.song_id = dsl.source_song_id;
+             
+     -- TODO: Drop and re-create other views here (yay)
     
     INSERT INTO migration (id, description)
-    VALUES ('{MIGRATION_ID}', 'Created duplicate song lookup table');
+    VALUES ('{MIGRATION_ID}', 'Created duplicate song lookup table and v_songs and v_archive views.');
     """)
