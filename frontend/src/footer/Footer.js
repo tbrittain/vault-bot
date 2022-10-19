1808e4969eb6779005d8714ea57b64e4411fcbb8
tbrittain <treybrittain@gmail.com>
2022-10-18 23:56:10 -0500

    Add changelog for 0.4.3

--- frontend/src/footer/Footer.js
+++ frontend/src/footer/Footer.js
@@ -25,7 +25,7 @@ const Footer = () => {
 					Report an issue
 				</a>
 				<Link to="/changelog" className={classes.link}>
-					Changelog (v0.4.2)
+					Changelog (v0.4.3)
 				</Link>
 			</Breadcrumbs>
 			<Copyright />
