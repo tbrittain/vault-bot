6be691a4d6baf81f8c384005c61e0a22abb66a7c
tbrittain <treybrittain@gmail.com>
2022-10-24 13:32:51 -0500

    Add is_explicit boolean to songs table #537

--- discord/src/db/migrations/albums_songs_rework.py
+++ discord/src/db/migrations/albums_songs_rework.py
@@ -2,4 +2,10 @@ MIGRATION_ID = "851b969c-68e2-41dd-9639-32c2c07f9f57"
 
 
 def migration_009(cur):
-    pass
+    cur.execute(f"""
+    ALTER TABLE songs
+    ADD COLUMN is_explicit BOOLEAN DEFAULT FALSE;
+    
+    INSERT INTO migration (id, description)
+    VALUES ('{MIGRATION_ID}', 'Added explicit bool to songs.');
+    """)
