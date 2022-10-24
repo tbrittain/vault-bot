38bab820f056079bb030261d53afaf975eeb648b
tbrittain <treybrittain@gmail.com>
2022-10-24 16:46:25 -0500

    Add rudimentary albums tables, including tracking various album art sizes #536 #539

--- discord/src/db/migrations/albums_songs_rework.py
+++ discord/src/db/migrations/albums_songs_rework.py
@@ -6,6 +6,31 @@ def migration_009(cur):
     ALTER TABLE songs
     ADD COLUMN is_explicit BOOLEAN DEFAULT FALSE;
     
+    CREATE TABLE albums
+    (
+        id VARCHAR(255),
+        name VARCHAR(255),
+        type VARCHAR(255),
+        album_art_small VARCHAR(255),
+        album_art_medium VARCHAR(255),
+        album_art_large VARCHAR(255),
+        release_date TIMESTAMP
+    );
+    
+    CREATE TABLE albums_artists
+    (
+        album_id VARCHAR(255) NOT NULL REFERENCES albums,
+        artist_id VARCHAR(255) NOT NULL REFERENCES artists
+    );
+    
+    CREATE TABLE songs_albums
+    (
+        song_id VARCHAR(255) NOT NULL REFERENCES songs,
+        album_id VARCHAR(255) NOT NULL REFERENCES albums
+    );
+    
+    -- To drop or not to drop artists_songs here?
+    
     INSERT INTO migration (id, description)
-    VALUES ('{MIGRATION_ID}', 'Added explicit bool to songs.');
+    VALUES ('{MIGRATION_ID}', 'Added explicit bool to songs, Added albums and albums_artists tables');
     """)
