1c98605a8f167fce797139f159796e93f92b74e3
tbrittain <treybrittain@gmail.com>
2022-10-24 22:17:13 -0500

    Drop old tables and columns in albums_songs_rework

--- discord/src/db/migrations/albums_songs_rework.py
+++ discord/src/db/migrations/albums_songs_rework.py
@@ -4,7 +4,7 @@ MIGRATION_ID = "851b969c-68e2-41dd-9639-32c2c07f9f57"
 def migration_009(cur):
     cur.execute(f"""
     ALTER TABLE songs
-    ADD COLUMN is_explicit BOOLEAN DEFAULT FALSE;
+        ADD COLUMN is_explicit BOOLEAN DEFAULT FALSE;
     
     CREATE TABLE albums
     (
@@ -29,7 +29,15 @@ def migration_009(cur):
         album_id VARCHAR(255) NOT NULL REFERENCES albums
     );
     
-    -- To drop or not to drop artists_songs here?
+    -- The below statements cause loss of data, which will be remedied by
+    -- a one-time migration, since we need to access the Spotify API to get
+    -- the missing data, and then some
+
+    ALTER TABLE songs
+        DROP COLUMN art,
+        DROP COLUMN album;
+        
+    DROP TABLE artists_songs;
     
     INSERT INTO migration (id, description)
     VALUES ('{MIGRATION_ID}', 'Added explicit bool to songs, Added albums and albums_artists tables');
